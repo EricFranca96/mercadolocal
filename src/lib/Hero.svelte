1982e22000c4928e1b3df991500ee5cf77bccf96
Eric França <ericprogramacao96@gmail.com>
2023-08-23 20:16:49 -0300

    svg

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,9 +1,15 @@
 <script>
-  import img from '$lib/assets/hero.jpg'
+  import img from "$lib/assets/hero.jpg"
+  import Ornamento1 from "$lib/assets/Ornamento1.svelte";
+  import Ornamento2 from "$lib/assets/Ornamento2.svelte";
 </script>
 
 <section class="hero">
   <img src={img} alt="Interior do Mercado Local">
+  <div class="ornamentos">
+    <Ornamento1/>
+    <Ornamento2/>
+  </div>
 </section>
 
 <style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,16 @@
   import Hero from "$lib/Hero.svelte";
 </script>
 
+<div class="painel">
+  
 <Hero/>
+
+</div>
+
+
+<style>
+  .painel{
+    position: relative;
+  }
+</style>
 
