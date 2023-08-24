a7d6ee4dd358d4dd93782c18eec08158279f0580
Eric França <ericprogramacao96@gmail.com>
2023-08-24 10:32:11 -0300

    .env

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,7 +1,10 @@
+import { PUBLIC_URL_DATABASE } from "$env/static/public";
+
+
 export const load = async ({fetch}) => {
 
     const pegarProdutos = async () => {
-      const res = await fetch('http://mercadolocal.test/json/api/produtos');
+      const res = await fetch(`${PUBLIC_URL_DATABASE}/json/api/produtos`);
       const produtos = await res.json();
       return produtos;
     }
